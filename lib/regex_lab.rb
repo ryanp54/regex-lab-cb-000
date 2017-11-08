c24da9749c1d8d88cc80559ed32b4f695cbe726f
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-08 22:49:00 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\A[A-Z].*\z\W)
+  text.match(/\A[A-Z].*\z\W/)
 end
 
 def valid_phone_number?(phone)
