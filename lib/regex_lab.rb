484ab7e3ac2b9fda3912793dd87e9d614ee0c19a
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-08 22:51:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,11 +3,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/[un]\w*[ing]/)
+  text.scan(/\W[un]\w*[ing]\W/)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\w{5}/)
+  text.scan(/\W\w{5}\W/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
